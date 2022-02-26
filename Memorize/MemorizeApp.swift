c36f9436f68e0cdb445ada6e959f084c585544c2
lvolkmann <landonvolkmann@gmail.com>
2022-02-26 13:35:09 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Landon Volkmann on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
